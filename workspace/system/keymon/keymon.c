9a139b842c8fe4ec4c82d4684e46f3635a8bc877
nknkol <31050940+nknkol@users.noreply.github.com>
2025-08-25 21:48:22 +0800

    Update key and button mappings for platform and keymon
    Standardized JOY_SELECT and JOY_START values, adjusted JOY_L3 and JOY_R3 mappings for 'is_brick' condition, and reassigned BTN_MOD_BRIGHTNESS and BTN_MOD_COLORTEMP to new keys. Updated CODE_MENU0 and CODE_MENU2 values in keymon.c to reflect new button assignments for color temperature and brightness control.

--- workspace/system/keymon/keymon.c
+++ workspace/system/keymon/keymon.c
@@ -22,9 +22,9 @@
 #define COLORTEMP_MIN 	0
 #define COLORTEMP_MAX 	40
 
-#define CODE_MENU0		7 //色温
+#define CODE_MENU0		314
 #define CODE_MENU1		315
-#define CODE_MENU2		6 //亮度
+#define CODE_MENU2		316
 #define CODE_PLUS		115
 #define CODE_MINUS		114
 #define CODE_MUTE		1
